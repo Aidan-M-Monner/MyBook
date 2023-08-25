3c042a8c95235fff59f81a7410062d11b3e584f6
Aidan-M-Monner <aidan.m.monner@gmail.com>
2023-08-25 16:48:50 -0500

    Designing login HTML (Part 5)

---
+++ login.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>MyBook - Login</title>
+        <link rel="stylesheet" type="text/css" href="assets/login.css">
+    </head>
+    <body>
+        <!----------- Top Bar ------------->
+        <div class="class-1">
+            <div class="class-2">MyBook</div>
+            <div class="class-3">Signup</div>
+        </div>
+
+        <!----------- Login Area ------------->
+        <div class="class-4">
+            Login to MyBook <br><br>
+            <input type="text" id="text"/><br><br>
+            <input type="text" id="text"/><br><br>
+            <input type="submit" id="button" value="Login"/>
+        </div>
+    </body>
+</html>
