8d7983752e802ff27de9d93515d9761df05c770d
Aidan-M-Monner <aidan.m.monner@gmail.com>
2023-08-30 12:14:57 -0500

    Building out profile (Part 11)

--- profile.php
+++ profile.php
@@ -4,11 +4,58 @@
         <link rel="stylesheet" type="text/css" href="assets/css/profile.css">
     </head>
     <body>
+        <!----------------- Top Bar ---------------------> 
         <div class="class-1">
             <div class="class-2">
                 MyBook &nbsp; &nbsp;
                 <input type="text" placeholder="Search for people..." class="class-3">
                 <img src="assets/img/selfie.jpg" class="class-4"/>
+            </div>
+        </div>
+
+        <!----------------- Cover Area ---------------------> 
+        <div class="class-5">
+            <div class="class-6">
+                <img src="assets/img/mountain.jpg" class="class-7"/>
+                <img src="assets/img/selfie.jpg" class="class-8"/><br>
+                <div class="class-9">Mary Banda</div><br>
+                <div class="class-10">Timeline</div> 
+                <div class="class-10">About</div> 
+                <div class="class-10">Friends</div> 
+                <div class="class-10">Photos</div> 
+                <div class="class-10">Settings</div>
+            </div>
+
+            <!----------------- Below Cover Area ---------------------> 
+            <div class="class-11">
+                <!----------------- Friends Area ---------------------> 
+                <div class="class-12">
+                    <div class="class-14">
+                        Friends <br>
+                        <div class="class-15">
+                            <img src="assets/img/user1.jpg" class="class-16"/><br>
+                            First User
+                        </div>
+
+                        <div class="class-15">
+                            <img src="assets/img/user2.jpg" class="class-16"/><br>
+                            Second User
+                        </div>
+
+                        <div class="class-15">
+                            <img src="assets/img/user3.jpg" class="class-16"/><br>
+                            Third User
+                        </div>
+
+                        <div class="class-15">
+                            <img src="assets/img/user4.jpg" class="class-16"/><br>
+                            Fourth User
+                        </div>
+                    </div>
+                </div>
+
+                <!----------------- Posts Area ---------------------> 
+                <div class="class-13"></div>
             </div>
         </div>
     </body>
