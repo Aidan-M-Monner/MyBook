5d1aa278ad62cedafae1a9ebfaa6016fbce6367a
Aidan-M-Monner <aidan.m.monner@gmail.com>
2023-09-21 12:10:35 -0500

    More cropping interface (Part 4)

--- Image-Resize/FaceBook-Crop/interface.php
+++ Image-Resize/FaceBook-Crop/interface.php
@@ -11,8 +11,8 @@
     #container {
         width: 400px;
         height: 400px;
-        /* border: solid 1px black; */
         margin: auto;
+        overflow: hidden;
     }
 
     #cropper {
@@ -22,12 +22,19 @@
         position: absolute;
         cursor: move;
     }
+
+    #range {
+        width: 400px;
+        margin: auto;
+        display: block;
+    }
 </style>
 
 <div id="container" onmousedown="mouseDown_on(event)" onmouseup="mouseDown_off(event)" onmouseenter="mouseMove_on(event)" onmouseleave="mouseMove_off(event)">
     <img id="image" src="image.jpg" style="width: 400px;">
     <div id="cropper"></div>
-</div>
+</div><br>
+<input id="range" type="range" min="10" max="40" onmousemove="resize_image(event)">
 <div id="info"></div>
 
 <script type="text/javascript">
@@ -35,6 +42,7 @@
     var image = document.getElementById("image");
     var container = document.getElementById("container");
     var cropper = document.getElementById("cropper");
+    var range = document.getElementById("range");
 
     var mouseMove = false;
     var mouseDown = false;
@@ -45,10 +53,17 @@
     var initImageY = 0;
 
     var ratio = 1;
-    reset_image();
+    var margin = 50;
 
     cropper.style.left = container.offsetLeft;
     cropper.style.top = container.offsetTop;
+
+    reset_image();
+
+    // Starting values
+    var originalImageWidth = image.clientWidth;
+    var originalImageHeight = image.clientHeight;
+
 
     window.onmousemove = function(event) {
         info.innerHTML = event.clientX + ":" + event.clientY; // Takes in mouse movements on x/y axis
@@ -57,8 +72,29 @@
             var x = event.clientX - initMouseX;
             var y = event.clientY - initMouseY;
 
-            x = initImageX + x; // current position of image + movement
+            // current position of image + movement
+            x = initImageX + x; 
             y = initImageY + y;
+
+            // Prevent image from going out of bounds
+            xlimit = container.clientWidth - image.clientWidth - margin;
+            ylimit = container.clientHeight - image.clientHeight - margin;
+
+            if(x > margin) { //Prevents Left
+                x = margin;
+            }
+
+            if(y > margin) { // Prevents Bottom
+                y = margin;
+            }
+
+            if(x < xlimit) { // Prevents Right
+                x = xlimit;
+            }
+
+            if(y < ylimit) { // Prevents Top
+                y = ylimit;
+            }
 
             image.style.left = x;
             image.style.top = y;
@@ -69,13 +105,63 @@
         mouseDown = false;
     }
 
+    function resize_image() {
+        // See initial location
+        var w = image.clientWidth;
+        var h = image.clientHeight;
+
+        // Change Image
+        image.style.width = (range.value / 10) * originalImageWidth;
+        image.style.height = (range.value / 10) * originalImageHeight;
+
+        // See how image is moving (zoom into center)
+        var w2 = image.clientWidth;
+        var h2 = image.clientHeight;
+
+        if(w - w2 != 0) {
+            var diff = (w - w2) / 2;
+            var diff2 = (h - h2) /2;
+            var x = (image.offsetLeft - container.offsetLeft) + diff;
+            var y = (image.offsetTop - container.offsetTop) + diff2;
+
+            // Prevent image from going out of bounds
+            xlimit = container.clientWidth - image.clientWidth - margin;
+            ylimit = container.clientHeight - image.clientHeight - margin;
+
+            if(x > margin) { //Prevents Left
+                x = margin;
+            }
+
+            if(y > margin) { // Prevents Bottom
+                y = margin;
+            }
+
+            if(x < xlimit) { // Prevents Right
+                x = xlimit;
+            }
+
+            if(y < ylimit) { // Prevents Top
+                y = ylimit;
+            }
+
+            image.style.left = x;
+            image.style.top = y;
+        }
+    }
+
     function reset_image() {
         if(image.naturalWidth > image.naturalHeight) {
             ratio = image.naturalWidth / image.naturalHeight;
-            image.style.height = container.clientHeight; // Make height as big as cropping space
-            image.style.width = container.clientWidth * ratio;
-            image.style.top = 0;
+            image.style.height = container.clientHeight - (margin * 2); // Make height as big as cropping space
+            image.style.width = (container.clientWidth - (margin * 2)) * ratio;
+            image.style.top = margin;
+
+            // Center image
+            var extra = (image.clientWidth - container.clientWidth) / 2;
+            image.style.left = extra * -1;
         }
+
+        range.value = 20;
     }
 
     function mouseDown_on() {
